e743957592d83e1ee621ce8f5d0ffacb7b5e565d
ViktorM02 <viktormann8@gmail.com>
2024-03-19 11:33:43 +0100

    Calibrate loadcell somewhat

--- software/loadcell/sketch_mar15a/sketch_mar15a.ino
+++ software/loadcell/sketch_mar15a/sketch_mar15a.ino
@@ -45,7 +45,7 @@ void setup() {
   Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
 						// by the SCALE parameter (not set yet)
 
-  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.set_scale(-45132.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();				        // reset the scale to 0
 
   Serial.println("After setting up the scale:");
@@ -70,9 +70,7 @@ void loop() {
   Serial.print("one reading:\t");
   Serial.print(scale.get_units(), 1);
   Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
-
-  scale.power_down();			        // put the ADC in sleep mode
-  delay(500);
-  scale.power_up();
+  Serial.println(scale.get_units(10), 2);
+		       
+  delay(100);
 }
