eaa13b1672448121f3866bedf63f1f8e02e34e3d
ViktorM02 <viktormann8@gmail.com>
2024-03-28 16:07:59 +0100

    Offset tared data

--- software/calculations/opn_readings.py
+++ software/calculations/opn_readings.py
@@ -2,22 +2,24 @@
 
 def open_test_data(file_name: str):
     f = open(file_name, "r")
-#print(f.readlines())
+
     no_tare_data = []
     tared_data = []
     tared = False
+    ix = 0
 
     for row in f:
         data = row.split()
         
         for i in data:
             if tared == False:
-                no_tare_data.append(i)
+                no_tare_data.append(float(i))
             else:
-                tared_data.append(i)
-        tared = True
+                tared_data.append(float(no_tare_data[ix]) - float(i))
+                ix+=1
+        tared = True #Second row has tared data
 
-    
+    print(tared_data)
 
 
 open_test_data("readings/05_1000.txt")
