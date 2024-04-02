282c2a5fa90fe604468a84d8584545e3dfe8b804
Axel Ljungqvist <axelljungqvist@gmail.com>
2024-04-02 15:46:44 +0200

    plot max cap graph with matplotlib

--- software/calculations/main.py
+++ software/calculations/main.py
@@ -1,16 +1,26 @@
 import os
+import matplotlib.pyplot as pyplot
 
 from opn_readings import open_test_data
 
-path = "readings/"
+forces = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
+
+data_path = "readings/"
 for thickness in ["05", "07", "10"]:
     for position in range(9):
-        for force in [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]:
+        maxes = []
+        for force in forces:
             data_file_name = thickness + "_" + str(position) + "_" + str(force) + ".txt"
-            if os.path.isfile(path + data_file_name):
+            if os.path.isfile(data_path + data_file_name):
                 print("file found: " + data_file_name)
 
-                caps = open_test_data(path + data_file_name)
+                caps = open_test_data(data_path + data_file_name)
 
-                print("max: " + str(max(caps)))
-                print("")
+                maxes.append(max(caps))
+
+        if(len(forces) == len(maxes)):
+            pyplot.plot(forces, maxes, linewidth=2.0)
+            pyplot.title("thickness: " + thickness + ", pos: " + str(position))
+            pyplot.show()
+
+            
